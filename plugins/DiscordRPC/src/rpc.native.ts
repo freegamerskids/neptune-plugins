ba342b1acba6ce896c500cf9a82acebd0e0fcc7e
freegamerskids <freegamerskids@gmail.com>
2024-09-03 21:38:28 +0200

    test logs

--- plugins/DiscordRPC/src/rpc.native.ts
+++ plugins/DiscordRPC/src/rpc.native.ts
@@ -1,14 +1,20 @@
 import { AutoClient, Presence } from "discord-auto-rpc";
 
-let client = new AutoClient({ transport: "ipc" });
+let client:AutoClient = new AutoClient({ transport: "ipc" });
+
+console.log("Is this working from the native part??????");
 
 client.on("ready", () => {
     console.log("Ready!");
 });
 
 export function load() {
+    console.log("Initializing...");
+
     client.endlessLogin({
         clientId: "1280598966471491594"
+    }).then(()=>{
+        console.log("Initialized!");
     });
 }
 

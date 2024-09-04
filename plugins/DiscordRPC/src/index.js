cce96eeeed74452449bdd130dd1a8b564c6836b5
freegamerskids <freegamerskids@gmail.com>
2024-09-04 22:00:14 +0200

    fix urls

--- plugins/DiscordRPC/src/index.js
+++ plugins/DiscordRPC/src/index.js
@@ -1,10 +1,8 @@
 import { load, unload, setActivity } from "./rpc.native";
 import { intercept } from "@neptune";
 
-console.log("Hello world!")
-
 function coverIdToUrl(id) {
-  return `https://resources.tidal.com/images/${id.replace('-','/')}/1028x1028.jpg`;
+  return `https://resources.tidal.com/images/${id.replaceAll('-','/')}/1028x1028.jpg`;
 }
 
 function getValuesForActivity() {
@@ -34,10 +32,7 @@ function getValuesForActivity() {
 ;(async ()=>{
   await load();
 
-  let vals = getValuesForActivity();
-  console.log(vals);
-
-  let state = window.neptune.store.getState();
+  console.log("[DiscordRPC] Loaded!");
 
   setActivity({
     type: 2,
@@ -45,41 +40,40 @@ function getValuesForActivity() {
     largeImageKey: "tidal",
   });
 
-  console.log(state.playQueue);
-})();
+  intercept("playbackControls/TIME_UPDATE", ([payload]) => {
+    let state = window.neptune.store.getState();
 
-intercept("playbackControls/TIME_UPDATE", ([payload]) => {
-  let state = window.neptune.store.getState();
-
-  let ts = Date.now();
-  let startTs = ts;
-  if (state.playbackControls.playbackState !== "NOT_PLAYING") {
-    if (payload !== 0) return false;
-  } else {
-    startTs = ts - (payload * 1000);
-  }
-
-  let vals = getValuesForActivity();
-  if (!vals) return false;
-
-  setActivity({
-    type: 2,
-    details: `Playing ${vals.title}`,
-    state: `by ${vals.artists}`,
-    smallImageKey: "tidal",
-    smallImageText: "Listening on TIDAL",
-    largeImageKey: vals.image,
-    largeImageText: vals.albumName,
-
-    timestamps: {
-      start: startTs,
-      end: ts + (vals.duration * 1000) - (payload * 1000),
+    let ts = Date.now();
+    let startTs = ts;
+    if (state.playbackControls.playbackState !== "NOT_PLAYING") {
+      if (payload !== 0) return false;
+    } else {
+      startTs = ts - (payload * 1000);
     }
-  });
-})
+
+    let vals = getValuesForActivity();
+    if (!vals) return false;
+
+    setActivity({
+      type: 2,
+      details: `Playing ${vals.title}`,
+      state: `by ${vals.artists}`,
+      smallImageKey: "tidal",
+      smallImageText: "Listening on TIDAL",
+      largeImageKey: vals.image,
+      largeImageText: vals.albumName,
+
+      timestamps: {
+        start: startTs,
+        end: ts + (vals.duration * 1000) - (payload * 1000),
+      }
+    });
+  })
+  
+})();
 
 export function onUnload() {
   unload();
 
-  console.log("Goodbye world!");
+  console.log("[DiscordRPC] Unloaded!");
 }
